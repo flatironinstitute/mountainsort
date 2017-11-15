86187434ccb7a3dde977572b838990343b189bc2
Jeremy Magland <jeremy.magland@gmail.com>
2017-11-15 14:51:28 -0500

    update pipeline

--- packages/pyms/curation/p_create_label_map.py
+++ packages/pyms/curation/p_create_label_map.py
@@ -11,7 +11,7 @@ from basic.p_extract_clips import extract_clips_helper
 from mlpy import readmda, writemda64, DiskReadMda
 
 processor_name='pyms.create_label_map'
-processor_version='0.1'
+processor_version='0.11'
 
 def create_label_map(*, metrics, label_map_out):
     """
@@ -46,10 +46,13 @@ def create_label_map(*, metrics, label_map_out):
             metrics_data['clusters'][idx]['metrics']['peak_snr'] < peak_snr_thresh:
             #Map to zero (mask out)
             label_map.append([0,metrics_data['clusters'][idx]['label']])
-        else: #Check if burst parent exists
-            if metrics_data['clusters'][idx]['metrics']['bursting_parent']:
-                label_map.append([metrics_data['clusters'][idx]['metrics']['bursting_parent'],
-                                  metrics_data['clusters'][idx]['label']])
+        elif metrics_data['clusters'][idx]['metrics']['bursting_parent']: #Check if burst parent exists
+            label_map.append([metrics_data['clusters'][idx]['metrics']['bursting_parent'],
+                              metrics_data['clusters'][idx]['label']])
+        else:
+            label_map.append([metrics_data['clusters'][idx]['label'],
+                              metrics_data['clusters'][idx]['label']]) # otherwise, map to itself!
+                
 
     #Writeout
     return writemda64(np.array(label_map),label_map_out)
