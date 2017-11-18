1ca29e0d99425997fd9820fe41ac1c8a78ef6a41
Witold Wysota <wysota@wysota.org>
2017-11-18 20:11:33 +0100

    Build debian package

--- packages/pyms/basic/basic_cpp.cpp
+++ packages/pyms/basic/basic_cpp.cpp
@@ -76,19 +76,16 @@ void extract_clips_(py::array_t<float> &clips_out,py::array_t<float> X, py::arra
 }
 
 
-/*
+
 PYBIND11_MODULE(basic_cpp,m)
 {
     m.def("extract_clips", &extract_clips_, "", py::arg().noconvert(),py::arg(),py::arg(),py::arg());
 }
-*/
 
+/*
 PYBIND11_PLUGIN(basic_cpp) {
     py::module m("basic_cpp");
     m.def("extract_clips", &extract_clips_, "", py::arg().noconvert(),py::arg(),py::arg(),py::arg());
     return m.ptr();
 }
-
-
-
-
+*/
