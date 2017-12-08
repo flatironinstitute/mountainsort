7103b4fa671bac83d45544e5c7194d46fec57444
Jeremy Magland <jeremy.magland@gmail.com>
2017-12-08 16:16:44 -0500

    fix isin problem

--- packages/pyms/curation/p_apply_label_map.py
+++ packages/pyms/curation/p_apply_label_map.py
@@ -11,7 +11,7 @@ from basic.p_extract_clips import extract_clips_helper
 from mlpy import readmda, writemda64, DiskReadMda
 
 processor_name='pyms.apply_label_map'
-processor_version='0.11'
+processor_version='0.12'
 
 def apply_label_map(*, firings, label_map, firings_out):
     """
@@ -33,12 +33,15 @@ def apply_label_map(*, firings, label_map, firings_out):
 
     #Propagate merge pairs to lowest label number
     for idx, label in enumerate(label_map[:,1]):
-        label_map[np.isin(label_map[:,0],label),0] = label_map[idx,0] # Input should be sorted
+    	# jfm changed on 12/8/17 because isin() is not isin() older versions of numpy. :)
+        #label_map[np.isin(label_map[:,0],label),0] = label_map[idx,0] # Input should be sorted
+        label_map[np.where(label_map[:,0]==label)[0],0] = label_map[idx,0] # Input should be sorted
 
     #Apply label map
     for label_pair in range(label_map.shape[0]):
+    	# jfm changed on 12/8/17 because isin() is not isin() older versions of numpy. :)
         #firings[2, np.isin(firings[2, :], label_map[label_pair, 1])] = label_map[label_pair,0]
-        firings[2, np.where(firings[2, :] == label_map[label_pair, 1])[0]] = label_map[label_pair,0] # jfm changed on 12/8/17 because isin() is not isin() older versions of numpy. :)
+        firings[2, np.where(firings[2, :] == label_map[label_pair, 1])[0]] = label_map[label_pair,0]
 
     #Mask out all labels mapped to zero
     firings = firings[:, firings[2, :] != 0]
